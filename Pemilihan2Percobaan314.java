a4d88402579c7d82be4ada218b702f8292caa852
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-10-05 21:42:55 +0700

    ekspedisi

--- Pemilihan2Percobaan314.java
+++ Pemilihan2Percobaan314.java
@@ -20,7 +20,7 @@ public class Pemilihan2Percobaan314 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasilan bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
