d0aa23f1a2091625585defdc061ec460bf89a68f
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-10-14 16:41:41 +0700

    percb2-js7

--- WhileGaji14.java
+++ WhileGaji14.java
@@ -17,18 +17,22 @@ public class WhileGaji14 {
             jabatan = scan.next();
             System.out.print("Masukkan jumlah jam lembur: ");
             jumlahJamLembur = scan.nextInt();
-            i++;
             
             if (jabatan.equalsIgnoreCase("direktur")) {
-                continue;
+                gajiLembur = 0;
             } else if (jabatan.equalsIgnoreCase("manajer")) {
                 gajiLembur = jumlahJamLembur * 100000;
             } else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahJamLembur * 75000;
+            } else {
+                System.out.println("Jabatan invalid");
+                continue;
             }
             
             totalGajiLembur += gajiLembur;
+            i++;
         }
+        
         
         System.out.println("Total gaji lembur: " + totalGajiLembur);
     }
