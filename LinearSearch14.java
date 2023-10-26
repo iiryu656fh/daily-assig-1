dc6c01674729955bf02e9856ebc5a7daa58106f0
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-10-27 06:08:21 +0700

    modif percb4

--- LinearSearch14.java
+++ LinearSearch14.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
 public class LinearSearch14 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int jmlhElemen = sc.nextInt();
         
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        int [] arrayInt = new int[jmlhElemen];
+        int key;
         int hasil = 0;
         
-        for (int i = 0; i < arrayInt.length; i++) {
+        for (int i = 0; i < jmlhElemen; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            arrayInt[i] = sc.nextInt();
+        }
+        
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+        
+        for (int i = 0; i < jmlhElemen; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
         
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        if (hasil != 0) {
+            System.out.println("Key ada di posisi indeks ke-"+hasil);
+        }
+        else 
+            System.out.println("Key tidak ditemukan");
     }
 }
