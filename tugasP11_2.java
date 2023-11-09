82bb7b6899666d5b67c8b18cde6cc16026bb39fd
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-09 19:48:29 +0700

    tugas2_js11

--- tugasP11_2.java
+++ tugasP11_2.java
@@ -3,14 +3,23 @@ public class tugasP11_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan nilai N(minimal 5): ");
-        int N = sc.nextInt();
+        boolean minimal = true;
         
-        for (int i = 1; i <= N; i++) {
-            for (int j = N; j >= i; j--) {
-                System.out.print("*");
+        while (minimal) {
+            System.out.print("Masukkan nilai N(minimal 5): ");
+            int N = sc.nextInt();
+            
+            if (N >= 5) {
+                for (int i = 1; i <= N; i++) {
+                    for (int j = N; j >= i; j--) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+                break;
+            } else {
+                System.out.println("Nilai N yang anda masukan kurang dari 5! coba lagi.");
             }
-            System.out.println();
         }
     }
 }
