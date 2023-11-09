9ab03c4bc416e8440626036cc50759b49c629984
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-09 19:25:24 +0700

    modif

--- NesteedLoop_2341720057.java
+++ NesteedLoop_2341720057.java
@@ -5,18 +5,16 @@ public class NesteedLoop_2341720057 {
         double[][] temps = new double[5][7];
         
         for (int i = 0; i < temps.length; i++) {
+            double totalTemps = 0;
             System.out.println("Kota ke-"+(i+1));
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-"+(j+1)+": ");
                 temps[i][j] = scanner.nextDouble();
+                totalTemps += temps[i][j];
             }
             System.out.println();
-        }
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke-"+(i+1)+": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
-            }
+            double rata2 = totalTemps / temps[i].length;
+            System.out.println("Rata-rata suhu kota ke-"+(i+1)+": "+rata2);
             System.out.println();
         }
     }
