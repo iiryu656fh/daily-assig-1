9a1f9678fa5561e4954e48783b4afb2c09dfbae4
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-23 08:50:25 +0700

    p1-js13

---
+++ TerimaKasih_14.java
@@ -0,0 +1,9 @@
+public class TerimaKasih_14 {
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the  teacher in the world! \n" 
+        +"You inspired in me a love for learning and make me feel like I could ask you anything.");
+    }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
--- tugasP11_3.java
+++ tugasP11_3.java
@@ -3,14 +3,27 @@ public class tugasP11_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Masukkan nilai N(minimal 3): ");
-        int N = sc.nextInt();
+        boolean minimal = true;
         
-        for (int i = 1; i <= N; i++) {
-            for (int j = N; j < i; j++) {
-                
+        while (minimal) {
+            System.out.print("Masukkan nilai N(minimal 3): ");
+            int N = sc.nextInt();
+            
+            if (N >= 3) {
+                for (int i = 1; i <= N; i++) {
+                    for (int j = 1; j <= N; j++) {
+                        if (i == 1 || i == N || j == 1 || j == N) {
+                            System.out.print(N);
+                        } else{
+                            System.out.print(" ");
+                        }
+                    }
+                    System.out.println();
+                }
+                break;
+            } else {
+                System.out.println("Nilai N yang anda masukkan kurang dari 3! Coba lagi.");
             }
-            System.out.println();
         }
     }
 }
