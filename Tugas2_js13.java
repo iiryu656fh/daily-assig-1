c7c3be0288c2f008d6400126fb7cb672e0bc9255
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-23 16:58:19 +0700

    modif tgs2js13

--- Tugas2_js13.java
+++ Tugas2_js13.java
@@ -3,22 +3,29 @@ public class Tugas2_js13 {
     //fungsi main
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = input.nextInt();
+        System.out.print("Masukkan jumlah tugas: ");
+        int jumlahTugas = input.nextInt();
+        
         //input nilai
-        int[][] nilai = inputNilai(input);
+        int[][] nilai = inputNilai(input, jumlahMahasiswa, jumlahTugas);
         //tampil nilai
         tampilNilai(nilai);
         //cari minggu ke-
-        int cariMingguKe = cariMinggu(nilai);
+        int cariMingguKe = cariMinggu(nilai, jumlahTugas);
         System.out.println("\nNilai tertinggi terdapat pada minggu ke-"+cariMingguKe);
         //cari mahasiswa
+        mahasiswaPintar(nilai);
         
     }
     //fungsi input nilai mahasiswa
-    static int[][] inputNilai(Scanner input) {
+    static int[][] inputNilai(Scanner input, int jumlahMahasiswa, int jumlahTugas) {
         int[][] nilai = new int[5][7];
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.println("Masukkan nilai Mahasiswa ke-"+(i+1)+": ");
-            for (int j =0; j < 7; j++) {
+            for (int j =0; j < jumlahMahasiswa; j++) {
                 System.out.print("Masukkan nilai minggu ke-"+(j+1)+": ");
                 nilai[i][j] = input.nextInt();
             }
@@ -27,21 +34,21 @@ public class Tugas2_js13 {
     }
     //fungsi tampilkan array nilai mahasiswa
     static void tampilNilai (int[][] nilai) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < nilai.length; i++) {
             System.out.print("Nilai mahasiswa ke-"+(i+1)+": ");
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print(nilai[i][j]+": ");
             }
             System.out.println();
         }
     }
     //fungsi Cari minggu keberapa terdapat nilai tertinggi
-    static int cariMinggu (int[][] nilai){
+    static int cariMinggu (int[][] nilai, int jumlahTugas){
         int N = 0, M = 0;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < jumlahTugas; i++) {
             int temp = 0;
-            for(int[] n : nilai) {
-                temp += n[i];
+            for(int j = 0; j < nilai.length; j++) {
+                temp += nilai[j][i];
             }
             if (temp > N) {
                 N = temp;
@@ -53,8 +60,8 @@ public class Tugas2_js13 {
     //fungsi Menampilkan mahasiswa dengan nilai tertinggi(minggu ke-)
     static void mahasiswaPintar(int[][] nilai) {
         int N = 0, Mhs = 0, Mg = 0;
-        for (int i = 0; i < 5; i++) {
-            for(int j = 0; j < 7; j++) {
+        for (int i = 0; i < nilai.length; i++) {
+            for(int j = 0; j < nilai[i].length; j++) {
                 if (nilai[i][j] > N) {
                     N = nilai [i][j];
                     Mhs = i + 1;
