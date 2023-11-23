8b75cc9d0ab60751e7390177eb46a234454c56ff
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-23 09:27:58 +0700

    p2 js13

--- TerimaKasih_14.java
+++ TerimaKasih_14.java
@@ -3,7 +3,12 @@ public class TerimaKasih_14 {
         System.out.println("Thank you for being the  teacher in the world! \n" 
         +"You inspired in me a love for learning and make me feel like I could ask you anything.");
     }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terima kasih Pak.. Bu.. Semoga sehat selalu.";
+        UcapanTambahan(ucapan);
     }
 }
