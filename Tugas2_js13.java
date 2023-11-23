b7c1405420dfadb3fd19e587166d2f08be94ed4c
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-23 16:40:16 +0700

    tugas2 js13

---
+++ Tugas2_js13.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class Tugas2_js13 {
+    //fungsi main
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //input nilai
+        int[][] nilai = inputNilai(input);
+        //tampil nilai
+        tampilNilai(nilai);
+        //cari minggu ke-
+        int cariMingguKe = cariMinggu(nilai);
+        System.out.println("\nNilai tertinggi terdapat pada minggu ke-"+cariMingguKe);
+        //cari mahasiswa
+        
+    }
+    //fungsi input nilai mahasiswa
+    static int[][] inputNilai(Scanner input) {
+        int[][] nilai = new int[5][7];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan nilai Mahasiswa ke-"+(i+1)+": ");
+            for (int j =0; j < 7; j++) {
+                System.out.print("Masukkan nilai minggu ke-"+(j+1)+": ");
+                nilai[i][j] = input.nextInt();
+            }
+        }
+        return nilai;
+    }
+    //fungsi tampilkan array nilai mahasiswa
+    static void tampilNilai (int[][] nilai) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Nilai mahasiswa ke-"+(i+1)+": ");
+            for (int j = 0; j < 7; j++) {
+                System.out.print(nilai[i][j]+": ");
+            }
+            System.out.println();
+        }
+    }
+    //fungsi Cari minggu keberapa terdapat nilai tertinggi
+    static int cariMinggu (int[][] nilai){
+        int N = 0, M = 0;
+        for (int i = 0; i < 5; i++) {
+            int temp = 0;
+            for(int[] n : nilai) {
+                temp += n[i];
+            }
+            if (temp > N) {
+                N = temp;
+                M = i+1;
+            }
+        }
+        return M;
+    }
+    //fungsi Menampilkan mahasiswa dengan nilai tertinggi(minggu ke-)
+    static void mahasiswaPintar(int[][] nilai) {
+        int N = 0, Mhs = 0, Mg = 0;
+        for (int i = 0; i < 5; i++) {
+            for(int j = 0; j < 7; j++) {
+                if (nilai[i][j] > N) {
+                    N = nilai [i][j];
+                    Mhs = i + 1;
+                    Mg = j + 1;
+                }
+            }
+        }
+        System.out.println("Mahasiswa yang mendaoatkan nilai tertinggi adalah mahasiswa ke-"+Mhs+" di minggu ke-"+Mg);
+    }
+}
