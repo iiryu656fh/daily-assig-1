10246cb7c47c5652b15a61a567d4366305b177bc
iiryu656fh <khoirotunnisa2507@gmail.com>
2023-11-30 08:55:18 +0700

    deret js14

--- Rekursif/Tugas1.java
+++ Rekursif/Tugas1.java
@@ -8,10 +8,25 @@ public class Tugas1 {
             return (n * DeretDescendingRekursif(n-1));
         }
     }
+    static int DeretDescendingIteratif (int n) {
+        int deret = 0;
+        for (int i = n; i >= 0; i--){
+            deret = i;
+            if (i == 0) {
+                System.out.print(deret);
+            } else
+                System.out.print(deret+", ");
+        }
+        return deret;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan bilangan: ");
         int bil = sc.nextInt();
+        System.out.print("Rekrusif: ");
         System.out.print(DeretDescendingRekursif(bil));
+        System.out.println();
+        System.out.print("Iteratif: ");
+        DeretDescendingIteratif(bil);
     }
 }
